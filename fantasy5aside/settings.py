1a0c346e93df3443173b58b6f1eb4d7b44e08412
Hughbert <hugh.mcpartlan1@gmail.com>
2016-12-02 09:52:50 +0000

    final push

--- fantasy5aside/settings.py
+++ fantasy5aside/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLLOWED_HOSTS =['fantasy-5-aside.herokuapp.com', 'localhost']
+ALLOWED_HOSTS =['fantasy-5-aside.herokuapp.com', 'localhost']
 
 # Application definition
 
